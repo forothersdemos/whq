bc297d9870a9d1755236bf337e9c7d9b76cf3544
xgwang <xgwang@sysnew.com>
2020-12-18 11:15:16 +0800

    api

--- src/pages/boxmedia.js
+++ src/pages/boxmedia.js
@@ -1,17 +1,43 @@
 const axios = require('axios');
+const XLSX = require('xlsx');
+const fs = require('fs');
+// 所有数据
+const queue = [];
 
 getData();
 
-function getData(pageIndex = 0, maxPage = 100) {
+/**
+ *
+ * @param pageIndex 开始遍历的 page
+ * @param maxPage 最大页数
+ */
+function getData(pageIndex = 1, maxPage = 100) {
     if (pageIndex > maxPage) {
         return
     }
-
+    // 获取数据
     axios({
-        url: 'https://api.boxmedia.vip/bizCards?page=2&biz_type=k_xhs&source=user&identity=kol'
+        url: `https://api.boxmedia.vip/bizCards?page=${pageIndex}&biz_type=k_xhs&source=user&identity=kol`
     }).then(res => res.data)
         .then(res => {
-            console.log(res);
+            // 追加存储所有数据
+            queue.push(...res.data);
+            console.log(queue.length);
+
+            // 解析需要的字段
+            let quotaJson = res.data.map(item => ({
+                city: item.city,
+                followers: item.followers,
+                name: item.name,
+                tags: item.tags
+            }));
+            console.log(quotaJson);
+
+            // 更新到本地 xlsx
+            const workbook = XLSX.utils.book_new();
+            const worksheet = XLSX.utils.json_to_sheet(quotaJson);
+            XLSX.utils.book_append_sheet(workbook, worksheet);
+            XLSX.writeFile(workbook, 'quota.xlsx');
         })
 
     getData(++pageIndex)
